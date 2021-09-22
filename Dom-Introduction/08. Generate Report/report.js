fde0a92929432329ba961a58de4e5d292768cf22
mihail-karabashev <mihailkarabashev@gmail.com>
2021-09-22 17:21:21 +0300

    up

--- Dom-Introduction/08. Generate Report/report.js
+++ Dom-Introduction/08. Generate Report/report.js
@@ -1,3 +1,20 @@
 function generateReport() {
-    //TODO
+    const checks = document.querySelectorAll('input[type="checkbox"]'); // 0 -6
+    const trElements = [...document.querySelectorAll('tbody>tr')]; // 0- 139
+    let arr = [];
+
+    trElements.forEach(trElement => {
+        let obj = {};
+        let tdElements = [...trElement.children];
+
+        tdElements.forEach((tdElement,i) => {
+            if (checks[i].checked) {
+                Object.assign(obj,{[checks[i].name]: tdElement.textContent});
+            }
+        });
+        arr.push(obj);
+    });
+
+
+    document.querySelector('#output').textContent =  JSON.stringify(arr,null,2);   
 }
