1b87aa3fb6e0c5b077cd62a2ee2e8e6ba6232aa6
mihail-karabashev <mihailkarabashev@gmail.com>
2021-09-24 11:09:27 +0300

    update

--- Dom- Manipulation/02. Add-Delete/app.js
+++ Dom- Manipulation/02. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    //TODO...
+    let ulElement = document.querySelector('#items');
+    let input = document.querySelector('input[type="text"]');
+
+
+    let liElement = document.createElement('li');
+    liElement.textContent = input.value;
+    let aElement = document.createElement('a');
+    aElement.href = '#';
+    aElement.textContent = '[Delete]';
+    liElement.appendChild(aElement);
+    ulElement.appendChild(liElement);
+
+   aElement.addEventListener('click', function(e){
+       e.target.parentNode.remove();
+   });
+
+    input.value = '';
 }
