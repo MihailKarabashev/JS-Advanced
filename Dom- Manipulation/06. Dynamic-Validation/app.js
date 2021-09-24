44efc2deadb0119acae6040e275902668d5ae410
mihail-karabashev <mihailkarabashev@gmail.com>
2021-09-24 14:28:18 +0300

    up

--- Dom- Manipulation/06. Dynamic-Validation/app.js
+++ Dom- Manipulation/06. Dynamic-Validation/app.js
@@ -1,3 +1,8 @@
 function validate() {
-    console.log('TODO:...');
+    let emailInput = document.querySelector('#email');
+    let pattern = /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+
+    emailInput.addEventListener('change', function(e){
+        pattern.test(e.target.value) == true ? e.target.classList.remove('error') : e.target.classList.add('error');
+    });
 }
--- Dom- Manipulation/07. Shopping-Cart/solution.js
+++ Dom- Manipulation/07. Shopping-Cart/solution.js
@@ -1,3 +1,26 @@
 function solve() {
-   //TODO...
+   let divElement = document.querySelector('div.shopping-cart');
+   let textArea = document.querySelector('textarea');
+  let sum = 0;
+  let arr = [];
+
+
+   divElement.addEventListener('click', function(e){
+      
+    if (e.target.classList.contains('add-product')) {
+      let money = Number(e.target.parentNode.nextElementSibling.textContent).toFixed(2);
+      let name = e.target.parentNode.previousElementSibling.children[0].textContent;
+      arr.push(name);
+      sum+=Number(money);
+      textArea.textContent+= `Added ${name} for ${money} to the cart.\n`;
+    }else if(e.target.classList.contains('checkout')){
+       arr.filter(unique);
+      textArea.textContent+= `You bought ${arr.join(', ')} for ${sum.toFixed(2)}.`;
+      [...document.querySelectorAll('button').forEach(x=> x.disabled = true)];
+    }
+   });
+   
+   const unique = (value, index, self) => {
+      return self.indexOf(value) === index
+    }
 }
