afead8208f21b23c89862ca7f80a91914921c19d
mihail-karabashev <mihailkarabashev@gmail.com>
2021-09-25 09:38:19 +0300

    up

--- Dom- Manipulation/Exercise/01. Sections/script.js
+++ Dom- Manipulation/Exercise/01. Sections/script.js
@@ -1,3 +1,22 @@
 function create(words) {
-   console.log('TODO:...');
+   let divContent = document.querySelector('#content');
+   words.forEach(word => {
+      let divElement = document.createElement('div');
+      let paragrathElement = document.createElement('p');
+      paragrathElement.textContent = word;
+      paragrathElement.style.display = 'none';
+
+      divElement.appendChild(paragrathElement);
+
+      divContent.appendChild(divElement);
+   });
+   
+   divContent.addEventListener('click', function(e){
+
+      if (e.target.tagName == 'DIV') {
+         e.target.children[0].style.display = 'block'
+         console.log(e.target);
+      }
+   });
+
 }
--- Dom- Manipulation/Exercise/03. Locked-Profile/app.js
+++ Dom- Manipulation/Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,25 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = [...document.querySelectorAll('button')];
+
+
+    buttons.forEach(button => {
+      button.addEventListener('click', function(e){
+
+         let myButton = e.target;
+
+         let locked = e.target.parentElement.querySelector('input[value="lock"');
+
+         let divElement = e.target.previousElementSibling;
+
+         if(divElement.style.display === '' && locked.checked === false){
+            divElement.style.display = 'block';
+            myButton.textContent = 'Hide it';
+
+        }else if(divElement.style.display === 'block' && locked.checked === false){
+         divElement.style.display = '';
+         myButton.textContent = 'Show more';
+        }
+
+      });
+    });
 }
