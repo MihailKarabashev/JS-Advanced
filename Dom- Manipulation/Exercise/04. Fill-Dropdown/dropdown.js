f235e23d2dba145fc809b04e3c5ad555c75d5b8c
mihail-karabashev <mihailkarabashev@gmail.com>
2021-09-25 10:23:12 +0300

    up

--- Dom- Manipulation/Exercise/04. Fill-Dropdown/dropdown.js
+++ Dom- Manipulation/Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+
+   let text = document.querySelector('#newItemText');
+   let value = document.querySelector('#newItemValue');
+
+   let selectElement = document.querySelector('#menu');
+
+   let optionElement = document.createElement('option');
+   optionElement.textContent = text.value + value.value;
+
+   selectElement.appendChild(optionElement);
+
+   text.value = '';
+   value.value = '';
 }
--- Dom- Manipulation/Exercise/05. Encode-and-Decode-Messages/app.js
+++ Dom- Manipulation/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,28 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+   
+    let main = document.querySelector('#main');
+
+    main.addEventListener('click', function(e){
+        let textAreas = e.currentTarget.querySelectorAll('div > textarea');
+       
+        if (e.target.textContent.startsWith('Encode')) {
+           let encodeText = textAreas[0].value.split('')
+           .map(x=> x.charCodeAt(0)+ 1)
+           .map(getCharCode).join('');
+
+           textAreas[0].value = '';
+           textAreas[1].value = encodeText;
+        }else{
+          let decodeText = textAreas[1].value.split('')
+          .map(x=>  x.charCodeAt(0) - 1)
+          .map(getCharCode)
+          .join('');
+
+          textAreas[1].value = decodeText;
+        }
+    });
+
+    function getCharCode(x){
+        return  String.fromCharCode(x);
+    }
 }
