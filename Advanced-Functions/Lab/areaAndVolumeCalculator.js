d827b161bd8889d3690fece84d6d731ae32be5b7
mihail-karabashev <mihailkarabashev@gmail.com>
2021-09-27 10:25:13 +0300

    up

---
+++ Advanced-Functions/Lab/areaAndVolumeCalculator.js
@@ -0,0 +1,28 @@
+
+function solve(area, vol, input) {
+
+   let parsedData = JSON.parse(input);
+   let arr = [];
+
+   parsedData.forEach(element => {
+      arr.push({area:area.call(element),volume: vol.call(element)});  
+   });
+   return arr;
+
+}
+ 
+function area() {
+    return Math.abs(this.x * this.y);
+};
+
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+};
+
+
+solve(area, vol, `[
+    {"x":"1","y":"2","z":"10"},
+    {"x":"7","y":"7","z":"10"},
+    {"x":"5","y":"2","z":"10"}
+    ]`
+);
